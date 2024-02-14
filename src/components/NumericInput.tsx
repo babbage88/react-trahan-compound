b94a114530e37210848989ed3108fb8ad1846dcd
unknown <justin.m.trahan@outlook.com>
2024-02-13 23:17:01 -0600

    Added styling and components

--- src/components/NumericInput.tsx
+++ src/components/NumericInput.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useRef } from 'react';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Input } from "@/components/ui/input"
 
 
@@ -10,16 +11,27 @@ interface NumericInputProps {
 }
 
 const NumericInput: React.FC<NumericInputProps> = ({ title, symbol, value, setValue }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleClick = () => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+      inputRef.current.select();
+    }
+  };
+
   return (
     <div className="flex flex-col gap-1">
       <h3>{title} ({symbol})</h3>
       <Input
         type="number"
         className=""
-        value={value}
+        defaultValue={value}
         onChange={(e) => {
           setValue(Number(e.target.value));
         }}
+        ref={inputRef}
+        onFocus={handleClick}
       />
     </div>
   );
