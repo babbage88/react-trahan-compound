cef51c12e4dac40b5b8f212383ed348730fb5a16
justin.m.trahan@outlook.com <justin.m.trahan@outlook.com>
2024-03-02 20:06:38 -0600

    Added currency formating to NumericInputs

--- src/components/NumericInput.tsx
+++ src/components/NumericInput.tsx
@@ -2,7 +2,7 @@ import React, { useRef } from 'react';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Input } from "@/components/ui/input";
 import { useEffect } from 'react';
-import { Label } from '@radix-ui/react-dropdown-menu';
+import { Label } from '@/components/ui/label';
 
 
 interface NumericInputProps {
@@ -17,6 +17,7 @@ interface NumericInputProps {
 
 const NumericInput: React.FC<NumericInputProps> = ({ title, symbol, value, className, placeholder, setValue  }) => {
   const inputRef = useRef<HTMLInputElement>(null);
+
 
   // Update input value when the value prop changes
   useEffect(() => {
@@ -33,12 +34,23 @@ const NumericInput: React.FC<NumericInputProps> = ({ title, symbol, value, class
     }
   };
 
+  const formatted = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+}).format(Number(value))
+
+  const isCurrency = symbol == '$'
+
   return (
     <div className={className}>
-      
-      <Label className='tracking-tight pl-3'>{title} ({symbol})</Label>
-      
+      <div className='flex felx-row justify-between'>
+        <Label className='tracking-tight pl-3 pb-2'>{title} ({symbol})</Label>
+        {isCurrency && 
+          <Label htmlFor={title} className='pb-2'>{formatted}</Label>
+        }
+      </div>
       <Input
+        id={title}
         type="number"
         className={"gap-1"}
         defaultValue={value}
